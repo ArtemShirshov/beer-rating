081ebcbcab442be8e49a2ac8524230822b0c2d40
a.shirshov <as.shirshov@icloud.com>
2021-08-31 16:14:18 +0300

    fix: remove CardActionArea

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -15,56 +14,54 @@ export const Item = ({
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={`static/images/${img}`}
-          title={title}
-        />
+      <CardMedia
+        className={classes.media}
+        image={`static/images/${img}`}
+        title={title}
+      />
 
-        <CardContent>
-          <Typography component="h2" variant="h5">
-            {title}
-          </Typography>
-          <Typography
-            color="textSecondary"
-            component="p"
-            gutterBottom
-            variant="body2"
-          >
-            {description}
-          </Typography>
+      <CardContent>
+        <Typography component="h2" variant="h5">
+          {title}
+        </Typography>
+        <Typography
+          color="textSecondary"
+          component="p"
+          gutterBottom
+          variant="body2"
+        >
+          {description}
+        </Typography>
 
-          <Rating value={Math.trunc(rating)} />
-          <Typography
-            color="textSecondary"
-            component="p"
-            gutterBottom
-            variant="body2"
-          >
-            Баллы: {rating}
-          </Typography>
+        <Rating value={Math.trunc(rating)} />
+        <Typography
+          color="textSecondary"
+          component="p"
+          gutterBottom
+          variant="body2"
+        >
+          Баллы: {rating}
+        </Typography>
 
-          <Typography
-            color="textSecondary"
-            component="p"
-            gutterBottom
-            variant="body2"
-          >
-            {note}
-          </Typography>
-          <Typography color="textSecondary" component="p" variant="body2">
-            Крепость: {fortress}%
-          </Typography>
-          <Typography color="textSecondary" component="p" variant="body2">
-            Объем: {volume}мл
-          </Typography>
-          <Typography color="textSecondary" component="p" variant="body2">
-            Регион: {region.country}
-            {region.city ? `, ${region.city}` : null}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        <Typography
+          color="textSecondary"
+          component="p"
+          gutterBottom
+          variant="body2"
+        >
+          {note}
+        </Typography>
+        <Typography color="textSecondary" component="p" variant="body2">
+          Крепость: {fortress}%
+        </Typography>
+        <Typography color="textSecondary" component="p" variant="body2">
+          Объем: {volume}мл
+        </Typography>
+        <Typography color="textSecondary" component="p" variant="body2">
+          Регион: {region.country}
+          {region.city ? `, ${region.city}` : null}
+        </Typography>
+      </CardContent>
     </Card>
   );
 };
