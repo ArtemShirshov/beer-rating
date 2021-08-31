605ba9c98273777af70f8c08813a1ffcabfb7a93
a.shirshov <as.shirshov@icloud.com>
2021-09-01 00:44:30 +0300

    feat: update header

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -8,6 +8,15 @@ export const useStyles = makeStyles({
   wrapper: {
     cursor: 'pointer',
     userSelect: 'none',
+    padding: '0 16px',
+    boxShadow: '0 4px 15px rgb(0 0 0 / 15%)',
+    background: 'white',
+    borderRadius: ' 0 0 4px 4px',
+    position: 'fixed',
+    zIndex: 1000,
+    maxWidth: 1232,
+    width: 'calc(100% - 32px)',
+    boxSizing: 'border-box',
   },
   icon: {
     marginRight: 8,
