a3e832e6e745599cf7fbda155e1fcdfeabb8c284
a.shirshov <as.shirshov@icloud.com>
2021-08-31 17:18:47 +0300

    refactor: optimize imports

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState } from 'react';
-import { BeerColor, Beer } from 'Icons';
+import { Beer, BeerColor } from 'Icons';
 
 import { useStyles } from './Header.styles';
 
